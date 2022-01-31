1802eeb263725874bc20c13c3b26b68c5c79639c
EmadEJ <s.emad.emamjomeh@gmail.com>
2022-01-31 16:09:10 +0330

    Improving the Graphic Logiv Handling the Campaign almost completely (logically and graphically) The game is technically playable

--- main.c
+++ main.c
@@ -3,21 +3,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <string.h>
 
-#define MAX_ISLANDS 25
 #define ISLAND_SIZE 150
-#define LOGO_SIZE 60
+#define LOGO_SIZE 50
 #define TROOP_SIZE 20
-#define MAX_TROOPS 1000
-#define MAX_CAMPAIGN
+#define MAX_ISLANDS 25
+#define MAX_TROOPS 2000
+#define MAX_CAMPAIGN 20
+#define MAX_SHAPE 1
+#define MAX_PLAYER 4
+#define MAX_FRAME 600000
+
 #define USERID 1
+
+// default perimeters
+#define FRAME_PER_PROD 60
+#define FRAME_PER_OUT 20
+#define TROOP_SPEED 3
+
+// Potion IDs
+#define FREEZE_ID 1
+#define HASTE_ID 2
+#define DECEPTION_ID 3
+#define WARCRY_ID 4
 
 const int SCREEN_WIDTH = 1500,SCREEN_HEIGHT = 1000;
 const int FPS=60;
-
-//////////// global variables
-
-////////////
 
 // for images (not made by me)
 SDL_Texture *getImageTexture(SDL_Renderer *sdlRenderer, char *image_path) {
@@ -37,7 +50,7 @@ SDL_Texture *getImageTexture(SDL_Renderer *sdlRenderer, char *image_path) {
     return texture;
 }
 
-/////////////////////// Useful general functions
+/////////////////////////////// Useful general functions
 // generates random between mn and mx
 int RAND(int mn, int mx){
     return mn+(rand()%(mx-mn));
@@ -55,27 +68,40 @@ int COLLIDE(int x1,int y1,int w1,int h1,int x2,int y2,int w2,int h2){
     return 1;
 }
 
+/////////////////////////////// Struct based functions
+
 // holds the data for an island
 struct Island{
     int troopsCount,capacity;
     int owner;
-    int shapeNo;
+    int shape;
     int isSelected;
     int x,y;
 };
 
 // holds the data for a troop
 struct Troop{
-    int x,y;
-    int xSpeed,ySpeed;
+    float x,y;
+    float xSpeed,ySpeed;
     int owner;
+    int dest;
 };
 
 // holds the data for a campaign (*Lashkar-Keshi)
 struct Campaign{
     int xStart,yStart;
     int xEnd,yEnd;
+    int count;
+    int dest;
     int owner;
+};
+
+// holds the data about a player
+struct Player{
+    int potion;
+    int potionLeft;
+    int islandCnt;
+    int troopCnt;
 };
 
 // holds the data about the map (the whole game actually)
@@ -84,12 +110,12 @@ struct Map{
     struct Island islandList[MAX_ISLANDS];
     int selectedIsland;
     int playerCnt;
+    struct Player playerList[MAX_PLAYER];
     int troopCnt;
     struct Troop troopList[MAX_TROOPS];
     int campaignCnt;
     struct Campaign campaignList[MAX_CAMPAIGN];
-};
-struct Map map;
+} map;
 
 void ADD_TROOP(struct Troop t){
     int ind=map.troopCnt;
@@ -112,16 +138,20 @@ void DESTROY_CAMPAIGN(int ind){
     map.campaignCnt--;
 }
 
+// checking if islands intersect
 int ISLAND_COLLIDE(int x1, int y1, int x2,int y2){
     int dis=(ISLAND_SIZE+LOGO_SIZE)/2;
     if( (x1<x2+dis && x1>x2-dis) && (y1<y2+dis && y1>y2-dis) ) return 1;
     return 0;
 }
 
-struct Map MAP_GENERATOR(int islandCnt, int enemyCnt){
+// generating a random map based
+struct Map MAP_GENERATOR(int islandCnt, int playerCnt){
     struct Map res;
     res.islandCnt=islandCnt;
     for(int i=0;i<islandCnt;i++){
+
+        // randomizing coords
         int x,y;
         int isok=0;
         while(!isok){
@@ -135,20 +165,29 @@ struct Map MAP_GENERATOR(int islandCnt, int enemyCnt){
                 }
             }
         }
+
+        res.islandList[i].shape= RAND(0,MAX_SHAPE);
         res.islandList[i].x=x;
         res.islandList[i].y=y;
         res.islandList[i].isSelected=0;
+        res.islandList[i].owner=0;
+        res.islandList[i].capacity=50;
+        res.islandList[i].troopsCount=0;
+        if(i<playerCnt){
+            res.islandList[i].owner= i+1;
+        }
     }
+
+    res.playerCnt=playerCnt;
     res.troopCnt=0;
     res.campaignCnt=0;
-    map.selectedIsland=-1;
+    res.selectedIsland=-1;
     return res;
 };
 
-void TROOP_COLLIDE(){
+////////////////////////////////// Game based functions
 
-}
-
+// handling clicking islands during the game
 void CLICKED(int x,int y){
     if(map.selectedIsland==-1){
         for(int i=0;i<map.islandCnt;i++){
@@ -156,6 +195,7 @@ void CLICKED(int x,int y){
                 if(map.islandList[i].owner==USERID){
                     map.islandList[i].isSelected=1;
                     map.selectedIsland=i;
+                    printf("Island Selected\n");
                 }
                 break;
             }
@@ -167,11 +207,15 @@ void CLICKED(int x,int y){
                 if(map.selectedIsland!=i){
                     struct Campaign c;
                     c.owner=USERID;
-                    c.xStart=map.islandList[map.selectedIsland].x;
-                    c.yStart=map.islandList[map.selectedIsland].y;
-                    c.xEnd=map.islandList[i].x;
-                    c.yEnd=map.islandList[i].y;
+                    c.xStart=map.islandList[map.selectedIsland].x+ISLAND_SIZE/2;
+                    c.yStart=map.islandList[map.selectedIsland].y+ISLAND_SIZE/2;
+                    c.xEnd=map.islandList[i].x+ISLAND_SIZE/2;
+                    c.yEnd=map.islandList[i].y+ISLAND_SIZE/2;
+                    c.count=map.islandList[map.selectedIsland].troopsCount;
+                    c.dest=i;
+                    map.islandList[map.selectedIsland].troopsCount=0;
                     ADD_CAMPAIGN(c);
+                    printf("Campaign Added from %d to %d\n", map.selectedIsland, i);
                 }
                 map.selectedIsland=-1;
                 map.islandList[i].isSelected=0;
@@ -181,10 +225,79 @@ void CLICKED(int x,int y){
     }
 }
 
-void MAP_UPDATE(){
+void MOVE_TROOPS(){
     for(int i=0;i<map.troopCnt;i++){
-
+        map.troopList[i].x+=map.troopList[i].xSpeed;
+        map.troopList[i].y+=map.troopList[i].ySpeed;
     }
+}
+
+struct Campaign HANDLE_CAMPAIGN(struct Campaign c, int frameNo){
+    if(frameNo%FRAME_PER_OUT==0){
+        struct Troop t;
+        t.owner=c.owner;
+        t.x=c.xStart;
+        t.y=c.yStart;
+        float dx=c.xEnd-c.xStart,dy=c.yEnd-c.yStart;
+        float k=sqrt((dx*dx+dy*dy)/(1.0*TROOP_SPEED*TROOP_SPEED));
+        t.xSpeed=dx/k;
+        t.ySpeed=dy/k;
+        t.dest=c.dest;
+        ADD_TROOP(t);
+        c.count--;
+    }
+    return c;
+}
+
+void PRODUCE_TROOPS(int frameNo){
+    for(int i=0;i<map.islandCnt;i++){
+        if(map.islandList[i].owner==0) continue ;
+        if(frameNo%FRAME_PER_PROD==0 || (map.playerList[map.islandList[i].owner].potion==WARCRY_ID && frameNo%FRAME_PER_PROD==0)){
+            if(map.islandList[i].troopsCount<map.islandList[i].capacity){
+                map.islandList[i].troopsCount++;
+            }
+        }
+    }
+}
+
+// updating the necessary parts of the map for each frame (movement, potion generation, troop production, islands state)
+void MAP_UPDATE(int frameNo){
+    // potion generation
+
+    // collide check (island to troop)
+    for(int i=0;i<map.troopCnt;i++){
+        int dest=map.troopList[i].dest;
+        if(COLLIDE(map.islandList[dest].x+(ISLAND_SIZE-LOGO_SIZE)/2, map.islandList[dest].y+(ISLAND_SIZE-LOGO_SIZE)/2, LOGO_SIZE, LOGO_SIZE, map.troopList[i].x, map.troopList[i].y, TROOP_SIZE, TROOP_SIZE)){
+            if(map.islandList[dest].owner==map.troopList[i].owner){
+                map.islandList[dest].troopsCount++;
+            }
+            else{
+                map.islandList[dest].troopsCount--;
+                if(map.islandList[dest].troopsCount<0){
+                    map.islandList[dest].owner=map.troopList[i].owner;
+                    map.islandList[dest].troopsCount=1;
+                }
+            }
+            DESTROY_TROOP(i);
+        }
+    }
+    // collide check (potion to troop)
+
+    // collide check (troop to troop)
+
+    // moving troops
+    MOVE_TROOPS();
+    // handling campaigns
+    for(int i=0;i<map.campaignCnt;i++){
+        map.campaignList[i]=HANDLE_CAMPAIGN(map.campaignList[i], frameNo);
+    }
+    for(int i=0;i<map.campaignCnt;i++){
+        while(i<map.campaignCnt && map.campaignList[i].count<=0){
+            DESTROY_CAMPAIGN(i);
+        }
+    }
+    // producing troops
+    PRODUCE_TROOPS(frameNo);
 }
 
 int main() {
@@ -197,34 +310,59 @@ int main() {
     SDL_Window *sdlWindow = SDL_CreateWindow("Test_window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
     SDL_Renderer *sdlRenderer = SDL_CreateRenderer(sdlWindow, -1, SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED);
 
+    // texture loading
+    SDL_Texture *islandShape[MAX_SHAPE][MAX_PLAYER+1];
+    for(int i=0;i<MAX_SHAPE;i++){
+        for(int j=0;j<=MAX_PLAYER;j++){
+            char str[40];
+            memset(str, 0, 40);
+            strcpy(str, "../Islands/Island");
+            str[strlen(str)]='0'+(i/10);
+            str[strlen(str)]='0'+(i%10);
+            str[strlen(str)]='0'+j;
+            strcat(str, ".bmp");
+            printf("%s\n", str);
+            islandShape[i][j]= getImageTexture(sdlRenderer, str);
+        }
+    }
     SDL_Texture *logo = getImageTexture(sdlRenderer, "../star.bmp");
     SDL_Texture *island = getImageTexture(sdlRenderer, "../island.bmp");
 
-    map= MAP_GENERATOR(20,3);
+    map= MAP_GENERATOR(10,3);
 
-    for(int i=0;i<map.islandCnt;i++){
-        printf("%d %d\n", map.islandList[i].x, map.islandList[i].y);
-    }
-
-    Sint16 xx=300,yy=300;
-
+    // game Loop
     SDL_bool shallExit = SDL_FALSE;
-    while(shallExit == SDL_FALSE) {
+    for(int frameNo=0;shallExit == SDL_FALSE;frameNo=(frameNo+1)%MAX_FRAME){
         SDL_SetRenderDrawColor(sdlRenderer, 0xff, 0xff, 0xff, 0xff);
         SDL_RenderClear(sdlRenderer);
+
+        // Updating everything
+        MAP_UPDATE(frameNo);
+
+        // map log
+        if(frameNo%FPS==0){
+/*            for(int i=0;i<map.islandCnt;i++){
+                printf("%d ", map.islandList[i].owner);
+            }
+            printf("\n");
+            for(int i=0;i<map.islandCnt;i++){
+                printf("%d ", map.islandList[i].troopsCount);
+            }
+            printf("\n\n");*/
+        }
 
         // displaying the islands
         for(int i=0;i<map.islandCnt;i++){
             SDL_Rect islandRect = {.x=map.islandList[i].x, .y=map.islandList[i].y, .w=ISLAND_SIZE, .h=ISLAND_SIZE};
-            SDL_RenderCopy(sdlRenderer, island, NULL, &islandRect);
+            SDL_RenderCopy(sdlRenderer, islandShape[map.islandList[i].shape][map.islandList[i].owner], NULL, &islandRect);
             SDL_Rect logoRect = {.x=map.islandList[i].x+(ISLAND_SIZE-LOGO_SIZE)/2, .y=map.islandList[i].y+(ISLAND_SIZE-LOGO_SIZE)/2, .w=LOGO_SIZE, .h=LOGO_SIZE};
             SDL_RenderCopy(sdlRenderer, logo, NULL, &logoRect);
         }
 
         //displaying the troops
-        xx+=2;
-        yy+=0;
-        filledCircleColor(sdlRenderer, xx, yy, 25, 0xeb3324ff);
+        for(int i=0;i<map.troopCnt;i++){
+            filledCircleColor(sdlRenderer, map.troopList[i].x, map.troopList[i].y, TROOP_SIZE/2, 0xfff05085);
+        }
 
         SDL_RenderPresent(sdlRenderer);
         SDL_Delay(1000 / FPS);
